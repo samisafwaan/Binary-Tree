4b11aecfb3aa4aa8391ef76369ac2a1766698f74
samisafwaan <150220360+samisafwaan@users.noreply.github.com>
2024-04-22 10:15:58 +0530

    Add files via upload

---
+++ Left View.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def left_view(root):
+    if root is None:
+        return
+    queue = [root]
+    while queue:
+        n = len(queue)
+        for i in range(n):
+            temp = queue.pop(0)
+            if i == 0:
+                print(temp.value, end=" ")
+            if temp.left:
+                queue.append(temp.left)
+            if temp.right:
+                queue.append(temp.right)
+                
+def build_tree(nodes):
+    if not nodes:
+        return None
+    root = TreeNode(nodes[0])
+    queue = [root]
+    i = 1
+    while queue:
+        current_node = queue.pop(0)
+        if i < len(nodes) and nodes[i] is not None:
+            current_node.left = TreeNode(nodes[i])
+            queue.append(current_node.left)
+        i += 1
+        if i < len(nodes) and nodes[i] is not None:
+            current_node.right = TreeNode(nodes[i])
+            queue.append(current_node.right)
+        i += 1
+    return root
+
+# Driver code
+if __name__ == "__main__":
+    nodes = list(map(int, input("Enter the values of the nodes separated by space: ").split()))
+    root = build_tree(nodes)
+    print("Left View:")
+    left_view(root)
---
+++ Top View.py
@@ -0,0 +1,52 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.hd = 0
+
+def top_view(root):
+    if root is None:
+        return
+    queue = []
+    hd_dict = {}
+    root.hd = 0
+    queue.append(root)
+    while queue:
+        temp = queue.pop(0)
+        hd = temp.hd
+        if hd not in hd_dict:
+            hd_dict[hd] = temp.value
+        if temp.left:
+            temp.left.hd = hd - 1
+            queue.append(temp.left)
+        if temp.right:
+            temp.right.hd = hd + 1
+            queue.append(temp.right)
+    for key in sorted(hd_dict.keys()):
+        print(hd_dict[key], end=" ")
+        
+def build_tree(nodes):
+    if not nodes:
+        return None
+    root = TreeNode(nodes[0])
+    queue = [root]
+    i = 1
+    while queue:
+        current_node = queue.pop(0)
+        if i < len(nodes) and nodes[i] is not None:
+            current_node.left = TreeNode(nodes[i])
+            queue.append(current_node.left)
+        i += 1
+        if i < len(nodes) and nodes[i] is not None:
+            current_node.right = TreeNode(nodes[i])
+            queue.append(current_node.right)
+        i += 1
+    return root
+
+# Driver code
+if __name__ == "__main__":
+    nodes = list(map(int, input("Enter the values of the nodes separated by space: ").split()))
+    root = build_tree(nodes)
+    print("Top View:")
+    top_view(root)
